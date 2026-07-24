c526aba4cd5483fc5b2171722cc9bf8512c2b178
Draedon <draedon123@gmail.com>
2026-07-25 01:58:28 +1000

    [e60vs] Change server port to 60

--- src/E60/Versus/server/main.cjs
+++ src/E60/Versus/server/main.cjs
@@ -4,7 +4,7 @@ const fs = require("fs");
 const { ServerCLI } = require("./cli.cjs");
 
 const IP = "127.0.0.1";
-const PORT = 443;
+const PORT = 60;
 
 const args = process.argv.slice(2);
 const autoStart = args.includes("start");
